18356cd10df02ef853c98d9e4fca8431f5a79b7c
Alexander Ratanas <alexanderratanas@gmail.com>
2022-04-20 15:19:33 -0500

    completed required goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,28 @@
-console.log('***** Cart Functions *****');
+console.log("***** Cart Functions *****");
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+const addItem = (item) => {
+  basket.push(item);
+  return true;
+};
+
+const listItems = (items) => {
+  for (item of items) {
+    console.log(item);
+  }
+};
+
+const empty = () => {
+  basket = [];
+  return true;
+};
+
+console.log(`Basket is ${basket}`);
+console.log("Adding bananas (expect true)", addItem("bananas"));
+console.log("Adding apple (expect true)", addItem("apple"));
+console.log("Adding grapes (expect true)", addItem("grapes"));
+console.log(`Basket is now ${basket}`);
+console.log("Emptying basket (expect true)", empty(basket));
+console.log(`Basket is now ${basket}`);
