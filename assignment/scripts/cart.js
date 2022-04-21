4c39ee2a55d8a743425a8a273cdd0ccf0bf0f24d
Alexander Ratanas <alexanderratanas@gmail.com>
2022-04-20 19:12:53 -0500

    strech goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -34,6 +34,18 @@ const empty = () => {
   return true;
 };
 
+const removeItem = (item) => {
+  let deletedItem = basket.indexOf(item);
+  if (deletedItem === -1) {
+    console.log(`You do not have ${item} in your basket`);
+    return false;
+  } else {
+    basket.splice(deletedItem, 1);
+    console.log(`${item} removed from your basket`);
+    return true;
+  }
+};
+
 console.log(`Basket is ${listItems(basket)}`);
 console.log("Adding bananas (expect true)", addItem("bananas"));
 console.log("Adding apple (expect true)", addItem("apple"));
@@ -44,3 +56,17 @@ console.log("Adding blue berries (expect false)", addItem("blue berries"));
 console.log(`Basket is now ${listItems(basket)}`);
 console.log("Emptying basket (expect true)", empty(basket));
 console.log(`Basket is now ${listItems(basket)}`);
+console.log(
+  "Adding ginger bread house (expect true)",
+  addItem("ginger bread house")
+);
+console.log("Adding candy canes (expect true)", addItem("candy canes"));
+console.log("Adding egg nog (expect true)", addItem("egg nog"));
+console.log(
+  "Removing candy canes from basket (expect true)",
+  removeItem("candy canes")
+);
+console.log(
+  "Removing pumpkins from basket (expect false)",
+  removeItem("pumpkins")
+);
